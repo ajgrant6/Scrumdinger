9d20ed532c3be759ff1d28dfbf88032e99f28760
AJ Grant <77135564+ajgrant6@users.noreply.github.com>
2022-11-06 22:11:50 -0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by AJ Grant on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
