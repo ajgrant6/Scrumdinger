53a29d1d5afc12d1421e825b7ea5ae49c9721f5e
AJ Grant <77135564+ajgrant6@users.noreply.github.com>
2022-11-10 17:22:06 -0800

    Creating a navigation heirarchy

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -11,15 +11,28 @@ struct ScrumsView: View {
     var scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
+        List {
+            ForEach(scrums){ scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)){
+                    CardView(scrum: scrum)
+                }
                 .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar{
+            Button(action: {}){
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
         }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.sampleData)
+        NavigationView{
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
     }
 }
